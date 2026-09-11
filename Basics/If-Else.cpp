b170d5503efdefdc68cf77bb8f96da613881449e
Himani Jangid <himani.11.jangid@gmail.com>
2026-09-11 16:54:04 +0530

    Add program

---
+++ Basics/If-Else.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter your age: ";
+    cin>>age;
+    if(age>=18){
+        cout<<"Adult";
+    }
+    else{
+        cout<<"Not Adult";
+    }
+    return 0;
+}
